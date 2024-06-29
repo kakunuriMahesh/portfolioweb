4d735e7f0b747d934b2177b95c4d88671e0af925
Mahesh Kakunuri <maheshkakunuri9@gmail.com>
2024-06-29 15:08:16 +0530

    fixed bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
   };
 
   return (
-    <div className="z-50 fixed w-full flex justify-between items-center bg-gray-300">
+    <div className="z-50 fixed w-full flex justify-between items-center bg-gray-600">
       <div className="ml-2">
         <a className="text-xl cursor-pointer no-underline" href="#home">
           <h4 className="border-4 border-double border-black p-2 text-black m-1 bg-white rounded-lg">
@@ -23,13 +23,13 @@ const Header = () => {
         </a>
       </div>
       <ul
-        className={`flex list-none p-0 m-0 ${
+        className={`list-none p-0 m-0 ${
           menuOpen ? "block" : "hidden"
-        } md:flex flex-col absolute top-0 right-0 bg-red-600 h-[100vh] pt-10 w-[120px] text-center text-white`}
+        } md:flex md:flex-row flex-col absolute md:static top-0 right-0 md:top-auto md:right-auto bg-red-600 md:bg-transparent h-[100vh] md:h-auto pt-10 md:pt-0 w-[120px] md:w-auto text-center md:text-left text-white`}
       >
         <li className="m-2">
           <a
-            className="no-underline text-black text-lg hover:text-white"
+            className="no-underline text-black md:text-white text-lg hover:text-white"
             href="#home"
           >
             Home
@@ -37,7 +37,7 @@ const Header = () => {
         </li>
         <li className="m-2">
           <a
-            className="no-underline text-black text-lg hover:text-white"
+            className="no-underline text-black md:text-white text-lg hover:text-white"
             href="#about"
           >
             About
@@ -45,7 +45,7 @@ const Header = () => {
         </li>
         <li className="m-2">
           <a
-            className="no-underline text-black text-lg hover:text-white"
+            className="no-underline text-black md:text-white text-lg hover:text-white"
             href="#projects"
           >
             Projects
@@ -53,7 +53,7 @@ const Header = () => {
         </li>
         <li className="m-2">
           <a
-            className="no-underline text-black text-lg hover:text-white"
+            className="no-underline text-black md:text-white text-lg hover:text-white"
             href="#services"
           >
             Services
@@ -61,21 +61,20 @@ const Header = () => {
         </li>
         <li className="m-2">
           <a
-            className="no-underline text-black text-lg hover:text-white"
+            className="no-underline text-black md:text-white text-lg hover:text-white"
             href="#contact"
           >
             Contact
           </a>
         </li>
-        <li className=" absolute top-0 right-0 m-5 md:hidden cursor-pointer">
+        <li className="absolute top-0 right-0 m-5 cursor-pointer md:hidden">
           <RxCross2 onClick={closeMenu} />
         </li>
       </ul>
-      {/* <i className="fa-solid fa-bars fas cursor-pointer md:hidden" onClick={openMenu}></i> */}
       {menuOpen ? (
         <></>
       ) : (
-        <RiMenuFill className="cursor-pointer m-5" onClick={openMenu} />
+        <RiMenuFill className="cursor-pointer m-5 md:hidden text-white" onClick={openMenu} />
       )}
     </div>
   );
